464b204b3bfc5a40553ca2231ed8a4eb14e6be88
aanchal <itsmeaanchal45@gmail.com>
2019-02-15 16:16:12 +0530

    first commit

---
+++ data.py
@@ -0,0 +1,22 @@
+def Articles():
+    articles = [ 
+        {
+            'id' : 1 , 
+            'title' : 'Article one' ,
+            'body' : 'hello to first article' , 
+            'create_date' : '04-25-2019'
+        } , 
+        {
+            'id' : 2 , 
+            'title' : 'Article two' ,
+            'body' : 'hello to second article' , 
+            'create_date' : '07-23-2019'
+        } ,
+        {
+            'id' : 3 , 
+            'title' : 'Article three' ,
+            'body' : 'hello to third article' , 
+            'create_date' : '06-24-2019'
+        }
+        ]
+    return articles
